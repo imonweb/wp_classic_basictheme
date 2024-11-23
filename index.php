92a2038d5da9e0e6dbe980067c2f7bd39bbc5aa9
Imon <contact@imonweb.co.uk>
2024-11-23 01:19:58 +0000

    wp_query

--- index.php
+++ index.php
@@ -8,13 +8,14 @@ get_header(); ?>
  <div class="col-sm-8">
   <?php 
   if(have_posts()) : 
+
     while( have_posts() ) : the_post(); ?> 
-
+    
       <?php get_template_part('content', get_post_format()); ?>
-
-      
+    
     <?php endwhile;
-    endif;
+    
+  endif;
   ?>
  </div>
 
